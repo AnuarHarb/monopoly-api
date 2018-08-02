c4f7a63322fa9a9e7e2a7c824d6bd587e120a46e
Anuar Harb <anuarharb@hotmail.com>
2018-08-02 17:44:41 -0500

    player and tiles endpoints

--- server.js
+++ server.js
@@ -14,8 +14,15 @@ app.use(bodyParser.json());
 
 const services = require('./services');
 
+// Players
 app.get('/players', services.getPlayers);
+app.get('/players/:id', services.getSinglePlayer);
 app.post('/players', services.newPlayer);
+// tiles && cards
+app.get('/tiles', services.getTiles);
+app.get('/tiles/:id', services.getSingleTile);
+app.get('/fortune', services.getFortune);
+app.get('/arc', services.getArc);
 
 app
   .listen(PORT, () => console.log(`Ready to play Monopoly in PORT ${ PORT } !!`));
--- services.js
+++ services.js
@@ -2,6 +2,16 @@
 
 const fs = require('fs'); //file sistem
 
+Array.prototype.clean = function() {
+  var newArray = new Array();
+  for(const item of this){
+    if(item) {
+      newArray.push(item);
+    }
+  }
+    return newArray;
+   };
+
 const getPlayers = (request, response) => {
   let players = [];
   try {
@@ -9,6 +19,24 @@ const getPlayers = (request, response) => {
     const fileJson = JSON.parse(file);
     players = fileJson.game.players;
     response.status(200).send(players);
+  } catch (error) {
+    response.status(500).send('Error, mueres');
+  }
+}
+
+const getSinglePlayer = (request, response) => {
+  const playerId = Number(request.params.id);
+  let players = [];
+  try {
+    const file = fs.readFileSync('data/db.json');
+    const fileJson = JSON.parse(file);
+    players = fileJson.game.players;
+    const singlePlayer = players.map(player => {
+      if (player.id === playerId) {
+        return player;
+      }
+    });
+    response.status(200).send(singlePlayer.clean());
   } catch (error) {
     response.status(500).send('Error, mueres');
   }
@@ -33,4 +61,59 @@ const newPlayer = (request, response) => {
   }
 }
 
-module.exports = {getPlayers, newPlayer};
+const getTiles = (request, response) => {
+  let tiles = [];
+  try {
+    const file = fs.readFileSync('data/db.json');
+    const fileJson = JSON.parse(file);
+    tiles = fileJson.static.tiles;
+    response.status(200).send(tiles);
+  } catch (error) {
+    response.status(500).send('Error, mueres');
+  }
+}
+
+const getSingleTile = (request, response) => {
+  const tileId = Number(request.params.id);
+  let tiles = [];
+  try {
+    const file = fs.readFileSync('data/db.json');
+    const fileJson = JSON.parse(file);
+    tiles = fileJson.static.tiles;
+    const singleTiles = tiles.map(tile => {
+      if (tile.id === tileId) {
+        return tile;
+      }
+    });
+    response.status(200).send(singleTiles.clean());
+  } catch (error) {
+    console.log(error);
+    response.status(500).send('Error, mueres');
+  }
+}
+
+const getFortune = (request, response) => {
+  let fortune = [];
+  try {
+    const file = fs.readFileSync('data/db.json');
+    const fileJson = JSON.parse(file);
+    fortune = fileJson.static.fortune;
+    response.status(200).send(fortune);
+  } catch (error) {
+    response.status(500).send('Error, mueres');
+  }
+}
+
+const getArc = (request, response) => {
+  let arc = [];
+  try {
+    const file = fs.readFileSync('data/db.json');
+    const fileJson = JSON.parse(file);
+    arc = fileJson.static.arc;
+    response.status(200).send(arc);
+  } catch (error) {
+    response.status(500).send('Error, mueres');
+  }
+}
+
+module.exports = {getPlayers, newPlayer, getTiles, getFortune, getArc, getSinglePlayer, getSingleTile};
